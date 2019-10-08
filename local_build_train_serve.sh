8a1ed2e97efabbf8e9301daf8d16ffeba1a92a23
Steven Purcell <steven.purcell@g2lytics.com>
2019-10-08 11:12:17 -0500

    Test failing locally and in SageMaker.

--- local_build_train_serve.sh
+++ local_build_train_serve.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 docker build -t mlc .
-docker run --rm -v $(pwd)/local_test:/opt/ml mlc train
-docker run --rm -p 127.0.0.1:8080:8080 -v $(pwd)/local_test:/opt/ml mlc serve
+docker run --rm -d -v $(pwd)/local_test:/opt/ml mlc train
+docker run --rm -d -p 127.0.0.1:8080:8080 -v $(pwd)/local_test:/opt/ml mlc serve
